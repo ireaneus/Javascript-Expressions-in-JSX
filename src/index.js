995bb77d5790e9720d6441b0a02afd12f4a9b1c5
Ireaneus <bwdave@protonmail.com>
2022-04-26 14:49:37 -0600

    index.js Created a cool component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Grace Fellowship";
+const location = "Mountain Home Idaho";
+const sermon = "Hebrews Part 1 (mp3)";
+const dnum = {
+  1: "One God",
+  2: "Witness",
+  3: "Trinity",
+  4: "World",
+  5: "number",
+  6: "Man",
+  7: "Complete"
+};
+const num = Math.floor(Math.random() * 7) + 1;
+
+const number = `${num}: ${dnum[num]}`;
+
+ReactDOM.render(
+  <div>
+    <h1>{`${name} of ${location}`}</h1>
+    <p>Pastor Jack Hurley teached on {sermon} on Feb 12th 1987</p>
+    <p>The numbers in scripture have meaning {number}</p>
+  </div>,
+  document.getElementById("root")
+);
